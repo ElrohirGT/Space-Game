9a70e6c4b4affbd4f6001170c95a23d5a12f6655
ElrohirGT <elrohirgt@gmail.com>
2022-07-26 09:24:17 -0600

    Added a life counting system

--- SGame/Ship.java
+++ SGame/Ship.java
@@ -10,15 +10,19 @@ public class Ship extends Actor
 {
     private final int ROCKET_WIDTH = 64;
 
-    private int life = 3;
+    private int _life = 3;
+    private int _ultimateCount = 0;
 
     private ShipConfiguration _configuration;
     private IMovementBrain _movementBrain;
     private IGunBrain _gunBrain;
-    private ICollisionBrain _collisionBrain;
+    private ICollisionBrain<Ship> _collisionBrain;
 
-    private GreenfootImage _thrustRocketImage;
-    private GreenfootImage _stillRocketImage;
+    private GreenfootImage[] _thrustRocketImages;
+    private GreenfootImage[] _stillRocketImages;
+
+    // private GreenfootImage _thrustRocketImage;
+    // private GreenfootImage _stillRocketImage;
 
     public Ship(ShipConfiguration configuration)
     {
@@ -28,10 +32,18 @@ public class Ship extends Actor
         _gunBrain = configuration.getShipGun();
         turn(-90);//This sets the ship facing up
 
-        _thrustRocketImage = new GreenfootImage("rocket.png");
-        HelpMethods.scaleToWidth(_thrustRocketImage, ROCKET_WIDTH);
-        _stillRocketImage = new GreenfootImage("rocketStill.png");
-        HelpMethods.scaleToWidth(_stillRocketImage, ROCKET_WIDTH);
+        _thrustRocketImages = new GreenfootImage[_life+1];
+        _stillRocketImages = new GreenfootImage[_life+1];
+
+        for (int i = 0; i <= _life; i++) {
+            var thrustImage = new GreenfootImage("rocket" + i + "lives.png");
+            HelpMethods.scaleToWidth(thrustImage, ROCKET_WIDTH);
+            _thrustRocketImages[i] = thrustImage;
+
+            var stillImage = new GreenfootImage("rocketStill" + i + "lives.png");
+            HelpMethods.scaleToWidth(stillImage, ROCKET_WIDTH);
+            _stillRocketImages[i] = stillImage;
+        }
     }
 
     public boolean enemyColliding()
@@ -45,7 +57,7 @@ public class Ship extends Actor
 
     public void looseLife()
     {
-        if (--life <= 0) {
+        if (--_life <= 0) {
             endGame();
         }
     }
@@ -63,12 +75,12 @@ public class Ship extends Actor
     {
         if (_movementBrain.shouldMove())
         {
-            setImage(_thrustRocketImage);
+            setImage(_thrustRocketImages[_life]);
             _movementBrain.move(this);
         }
         else
         {
-            setImage(_stillRocketImage);
+            setImage(_stillRocketImages[_life]);
         }
         if (_gunBrain.shouldFire())
         {
